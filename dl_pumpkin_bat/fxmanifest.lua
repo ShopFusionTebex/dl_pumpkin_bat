9c496374a87974f452a36bda82e6db2550205fa0
ShopFusionTebex <mariusz.karauda@gmail.com>
2024-10-23 16:10:40 +0200

    Add files via upload

---
+++ dl_pumpkin_bat/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games {'gta5'}
+author 'ShopFusion (https://shopfusion.tebex.io/)'
+description 'Weapon Pumpkin Bat by Dont_Look'
+lua54 'yes'
+
+files{
+	'**/weaponarchetypes.meta',
+	'**/weaponanimations.meta',
+	'**/pedpersonality.meta',
+	'**/weapons.meta',
+}
+
+data_file 'WEAPON_METADATA_FILE' '**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' '**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' '**/weapons.meta'
+
+client_script 'cl_weaponNames.lua'
